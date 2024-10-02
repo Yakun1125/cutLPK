58b9bfc7909a49812bf9412bb9da18a113c85869
Yakun1125 <90632209+Yakun1125@users.noreply.github.com>
2024-10-02 16:57:08 -0400

    Update config.cmake

--- cutLPK/config.cmake
+++ cutLPK/config.cmake
@@ -1,12 +1,12 @@
 # Options
 option(USE_CUPDLP "Use cuPDLP-C" ON)
 option(USE_PDLP "Use PDLP" OFF)
-option(USE_GUROBI "Use Gurobi" OFF)
+option(USE_GUROBI "Use Gurobi" ON)
 
 # Paths
-set(EIGEN_INCLUDE_DIR "/usr/include/eigen3" CACHE PATH "Path to Eigen include directory")
+set(EIGEN_INCLUDE_DIR "/content/eigen-3.4.0" CACHE PATH "Path to Eigen include directory")
 set(CMAKE_CUDA_PATH "/usr/local/cuda" CACHE PATH "Path to CUDA installation")
-set(HIGHS_HOME "/home/yakun/HiGHS" CACHE PATH "Path to HiGHS installation")
-set(CUPDLP_ROOT "/home/yakun/.vs/kmeans_cupdlp" CACHE PATH "Path to cuPDLP-C installation")
-set(ORTOOLS_ROOT "/home/yakun/or-tools/v9.10" CACHE PATH "Path to OR-Tools installation")
-set(GUROBI_ROOT "/home/yakun/GUROBI/gurobi1102/linux64" CACHE PATH "Path to Gurobi installation")
+set(HIGHS_HOME "/content/HiGHS" CACHE PATH "Path to HiGHS installation")
+set(CUPDLP_ROOT "/content/kmeans_cupdlp" CACHE PATH "Path to cuPDLP-C installation")
+set(ORTOOLS_ROOT "" CACHE PATH "Path to OR-Tools installation")
+set(GUROBI_ROOT "/content/gurobi1102/linux64" CACHE PATH "Path to Gurobi installation")
